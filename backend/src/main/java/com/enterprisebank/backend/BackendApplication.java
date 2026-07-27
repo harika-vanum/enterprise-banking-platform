f976efd9e718176f2aa0eb73f02a0ccf1380ac86
harika-vanum <harika.vanum.dev@gmail.com>
2026-07-27 23:17:09 +0530

    Update Application.java

--- backend/src/main/java/com/enterprisebank/backend/BackendApplication.java
+++ backend/src/main/java/com/enterprisebank/backend/BackendApplication.java
@@ -1,12 +1,17 @@
 package com.enterprisebank.backend;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 
 		SpringApplication.run(BackendApplication.class, args);
 	}
