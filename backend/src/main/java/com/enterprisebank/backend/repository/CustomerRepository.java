6541671ccdafa653554ae1208eddac5b54f9cc37
harika-vanum <harika.vanum.dev@gmail.com>
2026-07-29 21:17:14 +0530

    Update CustomerRepository.java

--- backend/src/main/java/com/enterprisebank/backend/repository/CustomerRepository.java
+++ backend/src/main/java/com/enterprisebank/backend/repository/CustomerRepository.java
@@ -2,7 +2,6 @@ package com.enterprisebank.backend.repository;
 
 import com.enterprisebank.backend.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.authentication.jaas.JaasPasswordCallbackHandler;
 
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
 }
