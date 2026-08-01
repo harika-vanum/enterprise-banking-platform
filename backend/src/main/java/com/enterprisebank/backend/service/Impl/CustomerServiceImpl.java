862df6911ec58d67282df4297d4a28f54c3a3c7d
harika-vanum <harika.vanum.dev@gmail.com>
2026-08-02 00:24:54 +0530

    Complete Customer module with validation, exception handling and logging

--- backend/src/main/java/com/enterprisebank/backend/service/Impl/CustomerServiceImpl.java
+++ backend/src/main/java/com/enterprisebank/backend/service/Impl/CustomerServiceImpl.java
@@ -9,6 +9,8 @@ import com.enterprisebank.backend.repository.CustomerRepository;
 import com.enterprisebank.backend.service.CustomerService;
 import jakarta.websocket.server.ServerEndpoint;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -19,24 +21,34 @@ public class CustomerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
     private final CustomerMapper customerMapper;
+    private static final Logger log = LoggerFactory.getLogger(CustomerServiceImpl.class);
 
     @Override
     public CustomerResponse createCustomer(CustomerRequest request) {
+        log.info("Creating customer with email: {}", request.getEmail());
         Customer customer = customerMapper.toEntity(request);
         Customer savedCustomer = customerRepository.save(customer);
+
+        log.info("Customer created successfully with ID: {}", savedCustomer.getId());
+
         return customerMapper.toResponse(savedCustomer);
     }
 
     @Override
     public CustomerResponse getCustomerById(Long id) {
+        log.info("Fetching customer with ID: {}",id);
         Customer customer = customerRepository.findById(id)
-                .orElseThrow(() -> new CustomerNotFoundException("Customer not found with id: " + id));
+                .orElseThrow(() -> {
+                    log.warn("Customer not found with ID: {}", id);
+                    return new CustomerNotFoundException("Customer not found with id: " + id);
+                });
 
         return customerMapper.toResponse(customer);
     }
 
     @Override
     public List<CustomerResponse> getAllCustomers() {
+        log.info("Fetching all customers");
         return customerRepository.findAll()
                 .stream()
                 .map(customerMapper::toResponse)
@@ -45,9 +57,12 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Override
     public CustomerResponse updateCustomer(Long id, CustomerRequest request) {
-
+        log.info("Updating customer with ID: {}", id);
         Customer customer = customerRepository.findById(id)
-                .orElseThrow(() -> new CustomerNotFoundException("Customer not found with id: " + id));
+                .orElseThrow(() -> {
+                    log.warn("Customer not found with ID: {}", id);
+                    return new CustomerNotFoundException("Customer not found with id: " + id);
+                });
 
         customer.setFirstName(request.getFirstName());
         customer.setLastName(request.getLastName());
@@ -55,16 +70,20 @@ public class CustomerServiceImpl implements CustomerService {
         customer.setPhone(request.getPhone());
 
         Customer updatedCustomer = customerRepository.save(customer);
-
+        log.info("Customer updated successfully with ID: {}", id);
         return customerMapper.toResponse(updatedCustomer);
     }
 
     @Override
     public void deleteCustomer(Long id) {
-
+        log.info("Deleting customer with ID: {}", id);
         Customer customer = customerRepository.findById(id)
-                .orElseThrow(() -> new CustomerNotFoundException("Customer not found with id: " + id));
+                .orElseThrow(() -> {
+                    log.warn("Customer not found with ID: {}", id);
+                    return new CustomerNotFoundException("Customer not found with id: " + id);
+                });
 
+        log.info("Customer deleted successfully with ID: {}", id);
         customerRepository.delete(customer);
     }
 }
