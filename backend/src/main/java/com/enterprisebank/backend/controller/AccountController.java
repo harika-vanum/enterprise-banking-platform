795bda9c6e4874747fe220f0a64c06de78eb44bb
harika-vanum <harika.vanum.dev@gmail.com>
2026-08-02 15:17:31 +0530

    Create AccountController.java

---
+++ backend/src/main/java/com/enterprisebank/backend/controller/AccountController.java
@@ -0,0 +1,45 @@
+package com.enterprisebank.backend.controller;
+
+import com.enterprisebank.backend.dto.AccountRequest;
+import com.enterprisebank.backend.dto.AccountResponse;
+import com.enterprisebank.backend.service.AccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/accounts")
+public class AccountController {
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping
+    public ResponseEntity<AccountResponse> createAccount(
+            @RequestBody AccountRequest request) {
+
+        AccountResponse response = accountService.createAccount(request);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AccountResponse>> getAllAccounts() {
+        return ResponseEntity.ok(accountService.getAllAccounts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AccountResponse> getAccountById(@PathVariable Long id) {
+        return ResponseEntity.ok(accountService.getAccountById(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteAccount(@PathVariable Long id) {
+        accountService.deleteAccount(id);
+        return ResponseEntity.noContent().build();
+    }
+}
